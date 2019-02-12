2e6bb060aed4202b60cf5754794b34928a21d61b
Administrator <Administrator@WIN-RM3BD0VSD45>
2019-02-12 09:16:49 +0900

    MoiveListController 통합

--- Team_Movie/src/main/java/team_movie/movie/controller/MovieListController.java
+++ Team_Movie/src/main/java/team_movie/movie/controller/MovieListController.java
@@ -14,26 +14,86 @@ import org.springframework.web.servlet.ModelAndView;
 
 import team_movie.model.GenreBean;
 import team_movie.model.GenreDao;
+import team_movie.model.MovieBean;
+import team_movie.model.MovieDao;
 
 @Controller
 public class MovieListController {
 	
+	private static final String getPage ="body/main";
 	private static final String command ="/main.tm";
-	private static final String gotoPage ="main";
 	
 	@Autowired
 	@Qualifier("myGenreDao")
 	GenreDao genreDao;
 	
+	@Autowired
+	MovieDao movieDao;
 	
 	@RequestMapping(value=command, method=RequestMethod.GET)
-	public String doActionGet(){
+	public ModelAndView doActionGet(){
 		
-		System.out.println("컨트롤러 진입");
+		System.out.println("MainController doGet");
 		
+		
+		int[] num = new int[3]; 
+		for(int i = 0; i < num.length ; i++){
+			num[i] = (int)(Math.random() * 7) + 1;
+		    for(int j = 0; j < i; j ++){
+		        if(num[i] == num[j]){
+		            i--;
+		            break;
+		        }
+		    }
+		}
+		String genres = "";
+		for (int i=0; i<3; i++) {
+			switch (num[i]) {
+			case 1 : genres = genres + "코미디/";
+				break;
+			case 2 : genres = genres + "액션/";
+				break;
+			case 3 : genres = genres + "범죄/";
+				break;
+			case 4 : genres = genres + "스릴러/";
+				break;
+			case 5 : genres = genres + "공포/";
+				break;
+			case 6 : genres = genres + "드라마/";
+				break;
+			case 7 : genres = genres + "멜로/";
+				break;
+			}
+		}
+		
+		System.out.println("genres : " + genres);
+		
+		String[] genre = genres.split("/");
+		
+		for (int i=0; i<genre.length; i++) {
+			System.out.println("genre["+i+"]" + genre[i]);
+		}
+
+		ModelAndView mav = new ModelAndView();
+
+		List<MovieBean> movie = movieDao.GetMovieList();	
+		List<MovieBean> movieByGenre1 = movieDao.GetMovieListByGenre(genre[0]);	
+		List<MovieBean> movieByGenre2 = movieDao.GetMovieListByGenre(genre[1]);	
+		List<MovieBean> movieByGenre3 = movieDao.GetMovieListByGenre(genre[2]);	
+		
+		mav.addObject("movie", movie);
+		mav.addObject("movieByGenre1", movieByGenre1);
+		mav.addObject("movieByGenre2", movieByGenre2);
+		mav.addObject("movieByGenre3", movieByGenre3);
+		mav.addObject("genre1", genre[0]);
+		mav.addObject("genre2", genre[1]);
+		mav.addObject("genre3", genre[2]);
+		mav.setViewName(getPage);
+		
+		//GenreData Get
 		getGenreList();
 		
-		return "/body/" + gotoPage;
+		return mav;
 	}
 	
 	
