73afc1d37e80d1dcea1a26dc4185d3d07e53cc24
Administrator <Administrator@WIN-RM3BD0VSD45>
2019-02-15 18:15:39 +0900

    재업로드 AdminUserEditController

--- Team_Movie/src/main/java/team_movie/admin/controller/AdminUserEditController.java
+++ Team_Movie/src/main/java/team_movie/admin/controller/AdminUserEditController.java
@@ -1,8 +1,19 @@
 package team_movie.admin.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import team_movie.model.GenreBean;
+import team_movie.model.GenreDao;
+import team_movie.model.UserBean;
+import team_movie.model.UserDao;
 
 @Controller
 public class AdminUserEditController {
@@ -10,13 +21,32 @@ public class AdminUserEditController {
 	private static final String command ="adminUserEdit.tm";
 	private static final String getPage = "body/admin/adminUserEdit";
 	
+	@Autowired
+	@Qualifier("myGenreDao")
+	GenreDao genreDao;
+	
+	@Autowired
+	@Qualifier("myUserDao")
+	UserDao userDao;
 	
 	@RequestMapping(value=command, method=RequestMethod.GET)
-	public String doActionGet(){
+	public ModelAndView doActionGet(){
 		System.out.println("admin controller");
+		ModelAndView mav = new ModelAndView();
+		List<UserBean> userList = new ArrayList<UserBean>();
+		userList = userDao.GetAllUserList();
 		
 		
-		return getPage;
+		//GenreData Get
+		List<GenreBean> genreList = null;
+		genreList = genreDao.getGenreList();
+		mav.addObject("genreList", genreList);
+				
+		mav.addObject("userList", userList);
+		mav.setViewName(getPage);
+		
+		
+		return mav;
 	}
 	
 }
