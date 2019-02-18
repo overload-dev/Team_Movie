c391af4c59f5fca74603839fc073e89463c105c2
Administrator <Administrator@WIN-RM3BD0VSD45>
2019-02-18 10:33:09 +0900

    멤버십 에딧 컨트롤러 업로드

--- Team_Movie/src/main/java/team_movie/admin/controller/AdminMembershipEditController.java
+++ Team_Movie/src/main/java/team_movie/admin/controller/AdminMembershipEditController.java
@@ -1,8 +1,38 @@
 package team_movie.admin.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import team_movie.model.MembershipBean;
+import team_movie.model.MembershipDao;
 
 @Controller
 public class AdminMembershipEditController {
-
+	
+	private static final String command ="adminMembershipEdit.tm";
+	private static final String gotoPage ="body/admin/adminMembershipEdit";
+	
+	@Autowired
+	@Qualifier("myMembership")
+	MembershipDao membershipDao;
+	
+	
+	@RequestMapping(value=command, method=RequestMethod.GET)
+	public String doActionGet(Model model){
+		
+		List<MembershipBean> membershipList = new ArrayList<MembershipBean>();
+		
+		membershipList = membershipDao.GetMemberShipList();
+		
+		model.addAttribute("membershipList",membershipList);		
+		
+		return gotoPage;
+	}
 }
